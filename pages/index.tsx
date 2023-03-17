00f30098ce623c5d62dd62d24a9feb6922b50a99
liapanou <liapanou7@gmail.com>
2023-03-17 17:49:22 +0200

    fixed text and email modals, put labels in the tools , made the images to change randomly and an image modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,42 @@ import { useSettings } from "@/providers";
 import clsx from "clsx";
 import dynamic from "next/dynamic";
 import { default as p5Types } from "p5";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
+
+// gets random image from a list
+function getRandomImage() {
+  const arr: string[] = [
+    "/images/northern lights.jpeg",
+    "/images/1456237268.jpg",
+    "/images/greece-meteora.jpeg",
+    "/images/Most-beautiful-places-in-Greece.jpg",
+    "/images/Rhodes.jpg",
+    "/images/Santorini-min.jpg",
+    "/images/screen-shot-2018-07-11-at-5-06-55-pm-1531343396.png",
+    "/images/shutterstock_282952640-1024x683.jpg",
+    "/images/the-best-place-to-focus-in-a-landscape.jpg",
+    "/images/Zakynthos-Navagio-bay-Greek-island.jpg",
+    "/images/greece-in-pictures-beautfiul-places-to-photograph-mykonose-windmill.jpg",
+  ];
+
+  // get random index value
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  // get random item
+  const item = arr[randomIndex];
+
+  return item;
+}
+
+// makes a toast when you submit an email
+
+// const myPromise = function();
+
+// toast.promise(myPromise, {
+//   loading: "Loading",
+//   success: "Your drawing has been sent successfully",
+//   error: "Unsuccessful submission of your drawing",
+// });
 
 // Will only import `react-p5` on client-side
 const Sketch = dynamic(
@@ -28,7 +63,7 @@ export default function Page() {
   const ref = useRef<p5Types>(null);
   const settings = useSettings();
   const menuRef = useRef<HTMLDivElement>(null);
-
+  const randomImage = useMemo(() => getRandomImage(), []); // change image when the page redenders
   let canvas: p5Types.Element | HTMLCanvasElement;
   let menuWidthRef = useRef(0);
   let width: number;
@@ -67,59 +102,60 @@ export default function Page() {
   };
 
   // start of rules of paint backet
-  // const floodFill = (
-  //   p5: p5Types,
-  //   x: number,
-  //   y: number,
-  //   targetColor: any,
-  //   fillColor: any
-  // ) => {
-  //   x = p5.mouseX;
-  //   y = p5.mouseY;
-  //   console.log("floodFill called with", x, y, targetColor, fillColor);
-  //   const currentColor = p5.get(x, y);
-  //   if (!compareColors(currentColor, targetColor)) {
-  //     return; // stop if the current color is not the target color
-  //   }
-  //   p5.set(x, y, fillColor); // fill the current pixel with the fill color
-  //   floodFill(p5, x + 1, y, targetColor, fillColor); // fill the right neighbor
-  //   floodFill(p5, x - 1, y, targetColor, fillColor); // fill the left neighbor
-  //   floodFill(p5, x, y + 1, targetColor, fillColor); // fill the bottom neighbor
-  //   floodFill(p5, x, y - 1, targetColor, fillColor); // fill the top neighbor
-  // };
+  const floodFill = (
+    p5: p5Types,
+    x: number,
+    y: number,
+    targetColor: any,
+    fillColor: any
+  ) => {
+    x = p5.mouseX;
+    y = p5.mouseY;
+    console.log("floodFill called with", x, y, targetColor, fillColor);
+    const currentColor = p5.get(x, y);
+    if (!compareColors(currentColor, targetColor)) {
+      return; // stop if the current color is not the target color
+    }
+    p5.set(x, y, fillColor); // fill the current pixel with the fill color
+    floodFill(p5, x + 1, y, targetColor, fillColor); // fill the right neighbor
+    floodFill(p5, x - 1, y, targetColor, fillColor); // fill the left neighbor
+    floodFill(p5, x, y + 1, targetColor, fillColor); // fill the bottom neighbor
+    floodFill(p5, x, y - 1, targetColor, fillColor); // fill the top neighbor
+  };
 
-  // const compareColors = (c1: any, c2: any) => {
-  //   // compare two color values as arrays of [R, G, B, A]
-  //   let currentColor = c1;
-  //   let targetColor = c2;
-  //   if (!p5Instance) {
-  //     return false;
-  //   }
-  //   // compare colors and return result
-  //   for (let i = 0; i < 4; i++) {
-  //     if (currentColor[i] !== targetColor[i]) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // };
+  const compareColors = (c1: any, c2: any) => {
+    // compare two color values as arrays of [R, G, B, A]
+    let currentColor = c1;
+    let targetColor = c2;
+    if (!p5Instance) {
+      return false;
+    }
+    // compare colors and return result
+    for (let i = 0; i < 4; i++) {
+      if (currentColor[i] !== targetColor[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
 
-  // const colorBacket = () => {
-  //   if (p5Instance && fillBg === true && p5Instance.mouseIsPressed === true) {
-  //     let x = p5Instance.mouseX;
-  //     let y = p5Instance.mouseY;
-  //     const targetColor = p5Instance.get(p5Instance.mouseX, p5Instance.mouseY);
-  //     console.log(targetColor);
-  //     const fillColor = p5Instance.color(255, 0, 0);
-  //     floodFill(
-  //       p5Instance,
-  //       p5Instance.mouseX,
-  //       p5Instance.mouseY,
-  //       targetColor,
-  //       fillColor
-  //     );
-  //   }
-  // };
+  const colorBacket = () => {
+    if (p5Instance && fillBg === true && p5Instance.mouseIsPressed === true) {
+      let x = p5Instance.mouseX;
+      let y = p5Instance.mouseY;
+      const targetColor = p5Instance.get(p5Instance.mouseX, p5Instance.mouseY);
+      console.log(targetColor);
+      const fillColor = p5Instance.color(255, 0, 0);
+      console.log(targetColor);
+      floodFill(
+        p5Instance,
+        p5Instance.mouseX,
+        p5Instance.mouseY,
+        targetColor,
+        fillColor
+      );
+    }
+  };
 
   // end of rules of paint backet
 
@@ -142,7 +178,7 @@ export default function Page() {
   };
 
   return (
-    <div className=" grid h-screen w-screen grid-cols-[300px_2fr] bg-white ">
+    <div className=" grid h-screen w-screen grid-cols-[300px_2fr]  overflow-hidden bg-white ">
       <div
         ref={menuRef}
         className=" flex flex-col border-r border-black bg-zinc-400 py-2 px-2"
@@ -151,19 +187,20 @@ export default function Page() {
         <button
           id="clearCanvas"
           type="button"
-          className="mb-4 rounded-full border border-black p-2  hover:bg-yellow-200"
+          className=" mb-4 rounded-full border border-black p-2 hover:bg-yellow-200"
           onClick={() => clearCanvas()}
         >
           Clear canvas
         </button>
 
-        <div className="mb-8 flex items-center justify-center gap-4">
+        <div className="flex items-center justify-center gap-4 xl:mb-4 2xl:mb-8">
           <button
             type="button"
             className={clsx(
-              " h-fit w-fit rounded-full border border-black p-2 hover:bg-yellow-200",
+              "hovertext h-fit w-fit rounded-full border border-black p-2 hover:bg-yellow-200",
               { "bg-yellow-200": settings.tool === 10 }
             )}
+            data-hover="eraser"
             onClick={() => {
               setErase(true);
               setFillBg(false);
@@ -175,26 +212,29 @@ export default function Page() {
           <button
             type="button"
             className={clsx(
-              " h-fit w-fit rounded-full border border-black p-2 hover:bg-yellow-200",
+              "hovertext h-fit w-fit rounded-full border border-black p-2 hover:bg-yellow-200",
               { "bg-yellow-200": fillBg }
             )}
+            data-hover="paint backet"
             onClick={() => {
               setErase(false);
               settings.setMode(0);
               setFillBg(true);
-              // colorBacket();
+
+              colorBacket();
             }}
           >
             🪣
           </button>
         </div>
-        <div className="mb-8 flex items-center justify-center gap-4">
+        <div className=" flex items-center justify-center gap-4 xl:mb-4 2xl:mb-8">
           <button
             type="button"
             className={clsx(
-              "h-fit w-fit rounded-full border border-black p-2  hover:bg-yellow-200",
+              " hovertext h-fit w-fit rounded-full border border-black p-2  hover:bg-yellow-200",
               { "bg-yellow-200": settings.tool === 4 }
             )}
+            data-hover="pencil"
             onClick={() => {
               setErase(false);
               setFillBg(false);
@@ -206,9 +246,10 @@ export default function Page() {
           <button
             type="button"
             className={clsx(
-              " h-fit w-fit rounded-full border border-black p-2 hover:bg-yellow-200",
+              " hovertext h-fit w-fit rounded-full border border-black p-2 hover:bg-yellow-200",
               { "bg-yellow-200": settings.tool === 9 }
             )}
+            data-hover="paint brush"
             onClick={() => {
               setErase(false);
               setFillBg(false);
@@ -218,7 +259,7 @@ export default function Page() {
             🖌️
           </button>
         </div>
-        <div className="mb-8 flex items-center justify-center gap-4">
+        <div className=" flex items-center justify-center gap-4 xl:mb-4 2xl:mb-8">
           {/* The button to open modal */}
           <label
             htmlFor="my-modal-5"
@@ -228,17 +269,18 @@ export default function Page() {
               setFillBg(false);
             }}
             className={clsx(
-              "btn1 h-fit w-fit   border border-black bg-transparent p-2 hover:bg-yellow-200",
+              "hovertext btn1 h-fit w-fit   border border-black bg-transparent p-2 hover:bg-yellow-200",
               {
                 "bg-yellow-200": textModal,
               }
             )}
+            data-hover="text"
           >
             🇦
           </label>
 
           {/* Put this part before </body> tag */}
-          <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+
           <div className={clsx("modal", { "modal-open": textModal })}>
             <div className="modal-box z-50">
               <form
@@ -278,17 +320,19 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className="mb-8 flex  flex-col items-center justify-center gap-3">
-          <h2 className="mb-4">Select Color :</h2>
+        <div className="flex flex-col items-center  justify-center gap-3 xl:mb-4 2xl:mb-8">
+          <h2 className="mb-2 2xl:mb-4">Select Color :</h2>
           <input
             type="color"
+            className="hovertext"
+            data-hover="color palette"
             onChange={(evt) =>
               settings.setSelectedColor(evt.currentTarget.value)
             }
           />
         </div>
-        <div className="mb-8  flex flex-col items-center justify-center">
-          <h2 className="mb-4">Save your drawing :</h2>
+        <div className="mb-4 flex  flex-col items-center justify-center 2xl:mb-8">
+          <h2 className="mb-2 2xl:mb-4">Save your drawing :</h2>
           <button
             id="saveCanvas"
             type="button"
@@ -303,8 +347,8 @@ export default function Page() {
           </button>
         </div>
 
-        <div className="flex  flex-col items-center justify-center">
-          <h2 className="mb-4">Share your drawing :</h2>
+        <div className=" flex flex-col items-center justify-center">
+          <h2 className="mb-2 2xl:mb-4">Share your drawing :</h2>
           {/* The button to open modal */}
           <label
             htmlFor="my-modal-6"
@@ -314,15 +358,17 @@ export default function Page() {
               setFillBg(false);
             }}
             className={clsx(
-              "btn1 h-fit w-fit   border border-black bg-transparent p-2 hover:bg-yellow-200",
+              " hovertext btn1 h-fit w-fit   border border-black bg-transparent p-2 hover:bg-yellow-200",
               { "bg-yellow-200": emailModal }
             )}
+            data-hover="email"
           >
             📧
           </label>
+          {/* <Toaster /> */}
 
           {/* Put this part before </body> tag */}
-          <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+
           <div className={clsx("modal", { "modal-open": emailModal })}>
             <div className="modal-box z-50">
               <form
@@ -358,17 +404,40 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="mt-auto mb-8 flex flex-col items-center justify-center gap-4">
+        <div className="mt-auto flex flex-col items-center justify-center gap-4 xl:mb-36 2xl:mb-8">
           <h2 className="font-bold ">Drawing Goal</h2>
           <div className="blinking-border">
-            <picture>
-              <img
-                src="https://www.nyip.edu/images/cms/photo-articles/the-best-place-to-focus-in-a-landscape.jpg"
-                alt="Drawing Goal"
-                width={500}
-                height={500}
-              />
-            </picture>
+            {/* The button to open modal */}
+            <label htmlFor="my-modal-3">
+              <picture>
+                <img
+                  src={randomImage}
+                  alt="Drawing Goal"
+                  width={500}
+                  height={500}
+                />
+              </picture>
+            </label>
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+              <div className="modal-box relative h-fit w-fit ">
+                <label
+                  htmlFor="my-modal-3"
+                  className="btn-sm btn absolute right-2 top-2 border-none bg-transparent text-black hover:bg-transparent"
+                >
+                  ✕
+                </label>
+                <br />
+                <picture>
+                  <img
+                    className="h-max w-max "
+                    src={randomImage}
+                    alt="Drawing Goal"
+                  />
+                </picture>
+              </div>
+            </div>
           </div>
         </div>
       </div>
